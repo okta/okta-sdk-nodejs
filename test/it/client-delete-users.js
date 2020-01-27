5022557c8670e624ae2f9b06126a2e54e8708080
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2020-01-27 13:28:01 -0800

    WIP: Adds delete script for org management

---
+++ test/it/client-delete-users.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect;
+
+const okta = require('../../');
+const models = require('../../src/models');
+const utils = require('../utils');
+const collection = require('../../src/collection');
+let orgUrl = process.env.OKTA_CLIENT_ORGURL;
+
+if (process.env.OKTA_USE_MOCK) {
+  orgUrl = `${orgUrl}/list-users`;
+}
+
+const client = new okta.Client({
+  orgUrl: 'https://okta.trexcloud.com',
+  token: 'xxxxxxxxxxxx',
+  requestExecutor: new okta.DefaultRequestExecutor()
+});
+
+describe('client.listUsers().each()', () => {
+  it('should allow me to delete users', async () => {
+    await client.listUsers().each(async(user) => {  
+      if (user.profile.firstName.includes('gzfirst')) {
+      	console.log(user.profile.firstName);
+      	await (utils.cleanup(client, user));
+      }
+
+      expect(user).to.be.an.instanceof(models.User);
+    });
+  });
+});
+
+
+
