17a45aed22e657397264948b525e9bee7218b809
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2020-05-04 22:33:58 -0700

    Adds test failure checks

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -2,6 +2,7 @@
 
 # Add yarn to the $PATH so npm cli commands do not fail
 export PATH="${PATH}:$(yarn global bin)"
+export TEST_FAILURE=false
 
 if ! npm install; then
   echo "npm install failed! Exiting..."
@@ -9,4 +10,18 @@ if ! npm install; then
 fi
 
 npm run test:unit
+if [ $? -ne 0 ]; then
+  TEST_FAILURE=true
+  echo "------- [ERROR] Unit tests failed  -------"
+fi
+
 npm run test:integration
+if [ $? -ne 0 ]; then
+  TEST_FAILURE=true
+  echo "------- [ERROR] Integration tests failed  -------"
+fi
+
+if ${TEST_FAILURE}
+then
+   exit 1
+fi
