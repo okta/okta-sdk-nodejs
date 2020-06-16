9fa10f495c27500ed30b3b5151101cddd502ae77
Shuo Wu <wushuo2010@gmail.com>
2020-06-16 16:41:00 -0400

    chore: sync with latest openapi

--- src/models/CatalogApplicationStatus.js
+++ src/models/CatalogApplicationStatus.js
@@ -14,32 +14,18 @@
 /* THIS FILE IS AUTO-GENERATED - SEE CONTRIBUTOR DOCUMENTATION */
 
 var Resource = require('../resource');
-const OAuth2Actor = require('./OAuth2Actor');
+
 
 /**
- * @class OAuth2ScopeConsentGrant
+ * @class CatalogApplicationStatus
  * @extends Resource
- * @property { hash } _embedded
- * @property { hash } _links
- * @property { string } clientId
- * @property { dateTime } created
- * @property { OAuth2Actor } createdBy
- * @property { string } id
- * @property { string } issuer
- * @property { dateTime } lastUpdated
- * @property { string } scopeId
- * @property { OAuth2ScopeConsentGrantSource } source
- * @property { OAuth2ScopeConsentGrantStatus } status
- * @property { string } userId
  */
-class OAuth2ScopeConsentGrant extends Resource {
+class CatalogApplicationStatus extends Resource {
   constructor(resourceJson, client) {
     super(resourceJson, client);
-    if (resourceJson && resourceJson.createdBy) {
-      this.createdBy = new OAuth2Actor(resourceJson.createdBy);
-    }
+
   }
 
 }
 
-module.exports = OAuth2ScopeConsentGrant;
+module.exports = CatalogApplicationStatus;
