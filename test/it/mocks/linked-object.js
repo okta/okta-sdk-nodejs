5fed437534fcd45d5d41e15beb77d07f3b9e0320
Shuo Wu <wushuo2010@gmail.com>
2020-06-26 10:10:47 -0400

    test: fix link object fake names

--- test/it/mocks/linked-object.js
+++ test/it/mocks/linked-object.js
@@ -2,13 +2,13 @@ const faker = require('faker');
 
 module.exports = () => ({
   primary: {
-    name: faker.lorem.word(),
+    name: faker.name.firstName(),
     title: 'Manager',
     description: 'Manager link property',
     type: 'USER'
   },
   associated: {
-    name: faker.lorem.word(),
+    name: faker.name.firstName(),
     title: 'Subordinate',
     description: 'Subordinate link property',
     type: 'USER'
