31e985e991974631a2bd177e37f8471d8c8efd6c
Shuo Wu <wushuo2010@gmail.com>
2020-07-28 13:05:51 -0400

    fix: url generation issue - OKTA-317349

---
+++ test/it/user-expire-password.js
@@ -0,0 +1,72 @@
+const expect = require('chai').expect;
+const utils = require('../utils');
+const okta = require('../../src');
+let orgUrl = process.env.OKTA_CLIENT_ORGURL;
+
+if (process.env.OKTA_USE_MOCK) {
+  orgUrl = `${orgUrl}/user-change-password`;
+}
+
+const client = new okta.Client({
+  scopes: ['okta.users.manage'],
+  orgUrl: orgUrl,
+  token: process.env.OKTA_CLIENT_TOKEN,
+  requestExecutor: new okta.DefaultRequestExecutor()
+});
+
+describe('User API Tests', () => {
+  it('should expire a users password', async () => {
+    // 1. Create a user
+    const newUser = {
+      profile: utils.getMockProfile('user-expire-password'),
+      credentials: {
+        password: { value: 'Abcd1234' }
+      }
+    };
+
+    // Cleanup the user if user exists
+    await utils.cleanup(client, newUser);
+
+    let queryParameters = { activate : 'true' };
+    const createdUser = await client.createUser(newUser, queryParameters);
+    utils.validateUser(createdUser, newUser);
+
+    // 2. Expire the user's password
+    await createdUser.expirePassword();
+
+    // 3. Verify that password was expired
+    const expiredUser = await client.getUser(createdUser.id);
+    expect(expiredUser.status).to.equal('PASSWORD_EXPIRED');
+
+    // 4. Delete the user
+    await utils.cleanup(client, createdUser);
+  });
+
+  it('should expire a users password and return temp password', async () => {
+    // 1. Create a user
+    const newUser = {
+      profile: utils.getMockProfile('user-expire-temp-password'),
+      credentials: {
+        password: { value: 'Abcd1234' }
+      }
+    };
+
+    // Cleanup the user if user exists
+    await utils.cleanup(client, newUser);
+
+    let queryParameters = { activate : 'true' };
+    const createdUser = await client.createUser(newUser, queryParameters);
+    utils.validateUser(createdUser, newUser);
+
+    // 2. Expire the user's password
+    const jsonResponse = await createdUser.expirePasswordAndGetTemporaryPassword();
+
+    // 3. Verify that password was expired
+    const expiredUser = await client.getUser(createdUser.id);
+    expect(jsonResponse.tempPassword).to.not.be.null;
+    expect(expiredUser.status).to.equal('PASSWORD_EXPIRED');
+
+    // 4. Delete the user
+    await utils.cleanup(client, createdUser);
+  });
+});
