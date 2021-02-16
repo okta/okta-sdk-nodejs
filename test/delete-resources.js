b26cc2fc86872580e42e0a165adfeb24ddad6e70
Kostya <kostiantyn.drozdov@okta.com>
2021-02-16 15:56:13 +0200

    Added delete-resources.js to remove all test resources, to avoid test failures.

---
+++ test/delete-resources.js
@@ -0,0 +1,67 @@
+const okta = require('../');
+const utils = require('./utils');
+
+let orgUrl = process.env.OKTA_CLIENT_ORGURL;
+
+if (process.env.OKTA_USE_MOCK) {
+  orgUrl = `${orgUrl}/application-get-user`;
+}
+
+const client = new okta.Client({
+  scopes: ['okta.apps.manage', 'okta.users.manage'],
+  orgUrl: orgUrl,
+  token: process.env.OKTA_CLIENT_TOKEN,
+  requestExecutor: new okta.DefaultRequestExecutor()
+});
+
+describe('Clean all test resources', () => {
+  //Remove all test users
+  //cleanTestUsers();
+
+  //Remove all test groups
+  //cleanTestGroups();
+
+  //Remove all test applications
+  //cleanApplications();
+});
+
+function cleanApplications() {
+  client.listApplications().each(application =>{
+    (application.label.startsWith('node-sdk')) ?
+      utils.removeAppByLabel() :
+      console.log(`Skipped application to remove ${application.label}`)
+  });
+}
+
+function cleanTestUsers() {
+  client.listUsers().each(user => {
+    (user.profile.email.endsWith('okta.com')) ?
+      console.log(`Skipped user to remove ${user.profile.email}`) :
+      utils.deleteUser(user)
+  });
+}
+
+function cleanTestGroups() {
+  const url = `${client.baseUrl}/api/v1/groups`;
+  const request = {
+    method: 'get',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+  };
+
+  return client.http.http(url, request)
+    .then(response => response.text())
+    .then(bodyResponse => JSON.parse(bodyResponse))
+    .then(user => {
+      user.forEach(element =>{
+        (element.profile.name.startsWith('node-sdk')) ?
+          utils.cleanupGroup(client, element) :
+          console.log(`Skipped group to remove ${element.profile.name}`)
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
