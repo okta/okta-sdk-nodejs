d162175991946f3c3a268586a25cf4723f8a6b5c
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-02-18 12:26:58 +0200

    reduce blocklisted models filtering duplication

--- templates/index.js
+++ templates/index.js
@@ -186,7 +186,9 @@ js.process = ({spec, operations, models, handlebars}) => {
     const args = [];
 
     const operation = method.operation;
-
+    if (modelName === 'UserFactor') {
+      console.log(method)
+    }
     operation.pathParams.forEach(param => {
       const matchingArgument = method.arguments.filter(argument => argument.dest === param.name)[0];
       if (matchingArgument && matchingArgument.src) {
