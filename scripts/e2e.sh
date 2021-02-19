06a4d8ec896765c1cf0e20be8672ffc566d0d78a
Kostya <kostiantyn.drozdov@okta.com>
2021-02-19 13:08:27 +0000

    Enabled e2e tests, added script that will clean test resources in org after tests finished.
    OKTA-352089
    <<<Jenkins Check-In of Tested SHA: 8c38267b219f6642068ff5cd289869466f62e461 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-sdk-nodejs

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -5,7 +5,7 @@ source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/okta-sdk-nodejs/privateKey.pem ${OKTA_HOME}/${REPO}/scripts/privateKey.pem
 
 export OKTA_CLIENT_ORGURL=https://node-sdk.okta.com
-export OKTA_CLIENT_TOKEN="$(aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/okta-sdk-nodejs/apiKey /dev/stdout)"
+get_secret prod/devex/okta-sdk-nodejs_apiKey OKTA_CLIENT_TOKEN
 export OKTA_CLIENT_CLIENTID=0oa1jnkiuz6FCTchz4x7
 export OKTA_CLIENT_PRIVATEKEY=$(cat ${OKTA_HOME}/${REPO}/scripts/privateKey.pem)
 
@@ -14,8 +14,11 @@ export TEST_RESULT_FILE_DIR="${REPO}/test-reports"
 
 if ! yarn test:integration; then
   echo "Integration tests failed! Exiting..."
+  yarn aftertest
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
+
+yarn aftertest
 
 ls -al ${OKTA_HOME}/${REPO}/test-reports
 
---
+++ test/delete-resources.js
@@ -0,0 +1,88 @@
+const okta = require('../');
+const utils = require('./utils');
+
+let orgUrl = process.env.OKTA_CLIENT_ORGURL;
+
+if (process.env.OKTA_USE_MOCK) {
+  orgUrl = `${orgUrl}/application-get-user`;
+}
+
+const client = new okta.Client({
+  scopes: ['okta.apps.manage', 'okta.users.manage'],
+  orgUrl: orgUrl,
+  token: process.env.OKTA_CLIENT_TOKEN,
+  requestExecutor: new okta.DefaultRequestExecutor()
+});
+
+describe('Clean all test resources', () => {
+
+  cleanAuthorizationServers();
+
+  cleanTestUsers();
+
+  cleanTestGroups();
+
+  cleanApplications();
+
+  cleanInlineHooks();
+
+});
+
+async function cleanInlineHooks(){
+    const collection = await client.listInlineHooks();
+    collection.each(async (inlineHook) => {
+
+        await inlineHook.deactivate();
+        await inlineHook.delete();
+    })
+}
+
+
+function cleanAuthorizationServers() {
+    client.listAuthorizationServers().each(
+        authorizationServer => {
+            authorizationServer.delete();
+        }
+    );
+}
+
+function cleanApplications() {
+  client.listApplications().each(application =>{
+    (application.label === 'Node SDK Service App' || application.label === 'Bacon Service Client') ?
+       console.log(`Skipped application to remove ${application.label}`) :
+       utils.removeAppByLabel(client, application.label)
+  });
+}
+
+function cleanTestUsers() {
+  client.listUsers().each(user => {
+    (user.profile.email.endsWith('okta.com')) ?
+      console.log(`Skipped user to remove ${user.profile.email}`) :
+      utils.deleteUser(user)
+  });
+}
+
+function cleanTestGroups() {
+  const url = `${client.baseUrl}/api/v1/groups`;
+  const request = {
+    method: 'get',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+  };
+
+  return client.http.http(url, request)
+    .then(responce => responce.text())
+    .then(bodyResponce => JSON.parse(bodyResponce))
+    .then(user => {
+      user.forEach(element =>{
+        (element.profile.name === 'Everyone') ?
+           console.log(`Skipped group to remove ${element.profile.name}`) :
+           utils.cleanupGroup(client, element)
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
