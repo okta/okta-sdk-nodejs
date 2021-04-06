2dfa398afa28eed58a82f9c84e6d2dc730357dc7
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-04-06 18:45:26 +0300

    add type assertion test for client.create* method param

--- test/type/client.test-d.ts
+++ test/type/client.test-d.ts
@@ -2,7 +2,8 @@ import { expectError, expectType } from 'tsd';
 import { Response } from 'node-fetch';
 import { Client } from '../../src/types/client';
 import { Collection } from '../../src/types/collection';
-import { Application } from '../../src/types/models/Application';
+import { Application, ApplicationOptions } from '../../src/types/models/Application';
+import { InlineHook } from '../../src/types/models/InlineHook';
 
 
 const client = new Client();
@@ -16,3 +17,9 @@ expectError(client.deleteApplicationUser('appId', 'userId', {sendEmail: 0}));
 // Client methods return either Promise or Collection
 expectType<Promise<Response>>(client.deletePolicy('policyId'));
 expectType<Collection<Application>>(client.listApplications());
+
+// methods expecting body request parameters
+const appOptions: ApplicationOptions = {
+  name: 'oidc_app',
+};
+expectType<Promise<Application>>(client.createApplication(appOptions)):
