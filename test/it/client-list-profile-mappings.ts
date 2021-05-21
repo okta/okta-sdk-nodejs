3da06b715b607d03b7b440916679ab6391dbd11a
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-05-21 10:04:45 +0300

    add integration tests for new APIs

---
+++ test/it/client-list-profile-mappings.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import {
+  Client,
+  DefaultRequestExecutor,
+  ProfileMapping } from '@okta/okta-sdk-nodejs';
+
+let orgUrl = process.env.OKTA_CLIENT_ORGURL;
+
+if (process.env.OKTA_USE_MOCK) {
+  orgUrl = `${orgUrl}/client-list-profile-mappings`;
+}
+
+const client = new Client({
+  scopes: ['okta.clients.read'],
+  orgUrl: orgUrl,
+  token: process.env.OKTA_CLIENT_TOKEN,
+  requestExecutor: new DefaultRequestExecutor()
+});
+
+describe('client.listProfileMappings()', () => {
+  // OKTA-397861: update org configuration to enable the test
+  xit('should return a collection', async () => {
+    const collection = client.listProfileMappings();
+    const profileMappings: ProfileMapping[] = [];
+    await collection.each(profileMapping => profileMappings.push(profileMapping));
+    expect(profileMappings).to.be.an('array').that.is.not.empty;
+    expect(profileMappings.pop()).to.be.instanceOf(ProfileMapping);
+  });
+});
