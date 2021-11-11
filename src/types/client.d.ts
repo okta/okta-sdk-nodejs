6184d9bb4d3a6647338168082def4bac093f9e07
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-11-11 14:05:23 +0200

    make `defaultCacheMiddlewareResponseBufferSize` optional

--- src/types/client.d.ts
+++ src/types/client.d.ts
@@ -29,7 +29,7 @@ export declare class Client extends ParameterizedOperationsClient {
     privateKey?: string | Record<string, unknown>
     cacheStore?: CacheStorage,
     cacheMiddleware?: typeof defaultCacheMiddleware | unknown
-    defaultCacheMiddlewareResponseBufferSize: number | undefined,
+    defaultCacheMiddlewareResponseBufferSize?: number,
   });
 
   requestExecutor: RequestExecutor;
