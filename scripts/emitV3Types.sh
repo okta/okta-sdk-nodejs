ca5502707ecc14c7ae4e33f028364dddfa75dd80
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2022-03-22 19:46:53 +0100

    copy common typings before tsc run

--- scripts/emitV3Types.sh
+++ scripts/emitV3Types.sh
@@ -1,3 +1,6 @@
+# copy typings into location next to JS modules so tsc can resolve them
+cp src/types/*.d.ts src/
 tsc src/v3/**/*.ts --declaration
+rm src/*.d.ts
 rsync -r --include='*.d.ts' --exclude="*.js" --exclude="*.ts" src/v3/ src/types/v3
 find src/v3/ -type f ! -name "*.js" -delete
