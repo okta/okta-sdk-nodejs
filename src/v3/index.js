ae1d3ed96fc7a5044d4e7094cb4aa783f271918f
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2022-03-25 12:21:11 +0100

    preserve v3 namespace in JS output

--- src/v3/index.js
+++ src/v3/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /* tslint:disable */
 /* eslint-disable */
 /**
@@ -22,17 +22,14 @@
  * https://github.com/swagger-api/swagger-codegen.git
  * Do not edit the class manually.
  */
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./api"), exports);
-__exportStar(require("./configuration"), exports);
-__exportStar(require("./models"), exports);
+exports.v3 = void 0;
+const models = require("./models");
+const apis = require("./api");
+const config = require("./configuration");
+var v3;
+(function (v3) {
+    v3.model = models;
+    v3.api = apis;
+    v3.configuration = config;
+})(v3 = exports.v3 || (exports.v3 = {}));
