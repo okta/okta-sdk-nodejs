3b5f2bf49f00cc9c9f33947beafb8d9205dd6a6f
Denys Oblohin <denys.oblohin@okta.com>
2022-06-17 12:29:03 +0300

    .

--- src/client.js
+++ src/client.js
@@ -21,7 +21,7 @@ const DEFAULT_USER_AGENT = `${packageJson.name}/${packageJson.version} node/${pr
 const repoUrl = 'https://github.com/okta/okta-sdk-nodejs';
 const { OAuth } = require('./oauth');
 const { getAffectedResources } = require('./request-middleware');
-const { AuthenticatorApi, SchemaApi, UserTypeApi, InlineHookApi, ProfileMappingApi, DomainApi, LinkedObjectApi, SystemLogApi, FeatureApi, GroupApi, EventHookApi, NetworkZoneApi, ThreatInsightApi, OrgSettingApi, ApplicationApi } = require('./generated');
+const { AuthenticatorApi, SchemaApi, UserTypeApi, InlineHookApi, ProfileMappingApi, DomainApi, LinkedObjectApi, SystemLogApi, FeatureApi, GroupApi, EventHookApi, NetworkZoneApi, ThreatInsightApi, OrgSettingApi, ApplicationApi, AuthorizationServerApi } = require('./generated');
 const { createConfiguration } = require('./generated/configuration');
 const { ServerConfiguration } = require('./generated/servers');
 const { Observable } = require('./generated/rxjsStub');
@@ -122,6 +122,7 @@ class Client extends GeneratedApiClient {
     this.threatInsightApi = new ThreatInsightApi(configuration);
     this.orgSettingApi = new OrgSettingApi(configuration);
     this.applicationApi = new ApplicationApi(configuration);
+    this.authorizationServerApi = new AuthorizationServerApi(configuration);
   }
 }
 
