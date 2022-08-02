38f4bcb31e99c17095c541e397c688979493a63e
denys.oblohin <denys.oblohin@okta.com>
2022-08-02 15:05:33 +0300

    wip . , . test! test!! catch . . .

--- test/it/principal-rate-limit.ts
+++ test/it/principal-rate-limit.ts
@@ -19,35 +19,76 @@ const client = new Client({
 });
 
 describe('Principal Rate Limit API', () => {
-  describe('Create', () => {
-    let prl;
-    afterEach(async () => {
-      await client.principalRateLimitApi.deletePrincipalRateLimitEntity({
-        principalRateLimitId: prl.id,
-      });
+  it('should create entity, get, update and list entities', async () => {
+    // TIP: Deletion of Principal Rate Limit entity is not supported
+
+    // Get first SSWS token
+    const tokens = await client.apiTokenApi.listApiTokens();
+    const { value: { id } } = await tokens.next(); // get first item
+    const token = await client.apiTokenApi.getApiToken({
+      apiTokenId: id
     });
 
-    it('should create entity', async () => {
-      const tokens = await client.apiTokenApi.listApiTokens();
-      const { value: { id } } = await tokens.next(); // get first item
-      const token = await client.apiTokenApi.getApiToken({
-        apiTokenId: id
-      });
+    const mockPrl = {
+      principalId: token.id,
+      principalType: 'SSWS_TOKEN' as v3.PrincipalType,
+      defaultPercentage: 50,
+      defaultConcurrencyPercentage: 75,   
+    };
 
-      const mockPrl = {
-        principalId: token.id,
-        principalType: 'SSWS_TOKEN' as v3.PrincipalType,
-        defaultPercentage: 50,
-        defaultConcurrencyPercentage: 75,   
-      };
+    // Create
+    let prl: v3.PrincipalRateLimitEntity;
+    try {
       prl = await client.principalRateLimitApi.createPrincipalRateLimitEntity({
         entity: mockPrl,
       });
-      console.log(1, prl);
       expect(prl).to.be.instanceOf(v3.PrincipalRateLimitEntity);
       expect(prl).to.have.property('id');
+      expect(prl.principalId).to.equal(mockPrl.principalId);
+      expect(prl.defaultConcurrencyPercentage).to.equal(mockPrl.defaultConcurrencyPercentage);
+    } catch(e) {
+      // Principal Rate Limit entity already exists for this token
+      expect(e.status).to.equal(400);
+      expect(e.errorSummary).to.contain('Api validation failed: principalRateLimitMediated');
+
+      // Get entity by principalId
+      const prls = await client.principalRateLimitApi.listPrincipalRateLimitEntities({
+        filter: `principalId eq "${token.id}"`
+      });
+      expect(prls).to.be.instanceOf(Collection);
+      const { value } = await prls.next(); // get first item
+      prl = value;
+    }
+    
+    // Get
+    const prl2 = await client.principalRateLimitApi.getPrincipalRateLimitEntity({
+      principalRateLimitId: prl.id,
     });
+    expect(prl2).to.be.instanceOf(v3.PrincipalRateLimitEntity);
+    expect(prl2.id).to.equal(prl.id);
+    expect(prl2.principalId).to.equal(prl.principalId);
+    expect(prl2.defaultConcurrencyPercentage).to.equal(prl.defaultConcurrencyPercentage);
+    
+    // Update
+    prl2.defaultConcurrencyPercentage = prl2.defaultConcurrencyPercentage == 75 ? 80 : 75;
+    const updatedPrl = await client.principalRateLimitApi.updatePrincipalRateLimitEntity({
+      principalRateLimitId: prl2.id,
+      entity: prl2,
+    });
+    expect(updatedPrl).to.be.instanceOf(v3.PrincipalRateLimitEntity);
+    expect(updatedPrl.id).to.equal(prl2.id);
+    expect(updatedPrl.principalId).to.equal(prl2.principalId);
+    expect(updatedPrl.defaultConcurrencyPercentage).to.equal(prl2.defaultConcurrencyPercentage);
+
+    // List
+    // TIP: Filter is required
+    const prls = await client.principalRateLimitApi.listPrincipalRateLimitEntities({
+      filter: 'principalType eq "SSWS_TOKEN"'
+    });
+    expect(prls).to.be.instanceOf(Collection);
+    await prls.each(prl => {
+      expect(prl).to.be.instanceOf(v3.PrincipalRateLimitEntity);
+    });
+
   });
-
-
 });
