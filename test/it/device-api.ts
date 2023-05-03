f8ece55de87a635b826b5b4d03b5a50c469b3001
denys.oblohin <denys.oblohin@okta.com>
2023-05-03 16:05:27 +0300

    Add test

--- test/it/device-api.ts
+++ test/it/device-api.ts
@@ -8,18 +8,76 @@ const client = new Client({
   token: process.env.OKTA_CLIENT_TOKEN,
 });
 
+async function getFirstDevice() {
+  const collection = await client.deviceApi.listDevices({
+    limit: 1
+  });
+  const { value: device } = await collection.next(); // access the first item of the collect
+  return device;
+};
+
 describe('Device API', () => {
-  beforeEach(async () => {
-  });
-
-  afterEach(async () => {
-  });
-
   it('lists existing devices', async () => {
     const collection = await client.deviceApi.listDevices();
     for await (const device of collection) {
       expect(device).to.be.instanceOf(Device);
-      console.log(1, device);
+    }
+  });
+
+  it('get existing device by id', async () => {
+    const testDevice = await getFirstDevice();
+    const deviceFromGet = await client.deviceApi.getDevice({
+      deviceId: testDevice.id
+    });
+    expect(deviceFromGet).to.be.instanceOf(Device);
+    expect(deviceFromGet.id).to.equal(testDevice.id);
+  });
+
+  it('manage status of device', async () => {
+    let testDevice = await getFirstDevice();
+
+    // deactivate
+    if (testDevice.status === 'ACTIVE') {
+      await client.deviceApi.deactivateDevice({
+        deviceId: testDevice.id
+      });
+      testDevice = await client.deviceApi.getDevice({
+        deviceId: testDevice.id
+      });
+      expect(testDevice.status).to.equal('DEACTIVATED');
+    }
+
+    // activate
+    if (testDevice.status === 'DEACTIVATED') {
+      await client.deviceApi.activateDevice({
+        deviceId: testDevice.id
+      });
+      testDevice = await client.deviceApi.getDevice({
+        deviceId: testDevice.id
+      });
+      expect(testDevice.status).to.equal('ACTIVE');
+    }
+    
+    // suspend
+    if (testDevice.status === 'ACTIVE') {
+      await client.deviceApi.suspendDevice({
+        deviceId: testDevice.id
+      });
+      testDevice = await client.deviceApi.getDevice({
+        deviceId: testDevice.id
+      });
+      expect(testDevice.status).to.equal('SUSPENDED');
+    }
+
+    // unsuspend
+    if (testDevice.status === 'SUSPENDED') {
+      await client.deviceApi.unsuspendDevice({
+        deviceId: testDevice.id
+      });
+      testDevice = await client.deviceApi.getDevice({
+        deviceId: testDevice.id
+      });
+      expect(testDevice.status).to.equal('ACTIVE');
     }
   });
 });
