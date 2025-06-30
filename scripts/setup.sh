dd93af25c4dfaf509dd5dc6c7180f5e1dd452902
Denys Oblohin <denys.oblohin@okta.com>
2025-06-30 13:31:27 +0300

    log node install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Add yarn to the $PATH so npm cli commands do not fail
-export PATH="${PATH}:$(yarn global bin)"
-
 # Install required node version
 export NVM_DIR="/root/.nvm"
-setup_service node v18.19.0 &> /dev/null
+setup_service node v18.19.0
+
+if ! npm install -g yarn@1.22.22; then
+  echo "Failed to install yarn"
+  exit ${FAILED_SETUP}
+fi
+
+# Add yarn to the $PATH so npm cli commands do not fail
+export PATH="${PATH}:$(yarn global bin)"
 
 # Revert the cache-min setting, since the internal cache does not apply to
 # these repos (and causes problems in lookups)
