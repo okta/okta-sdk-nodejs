3cb719a92802a19013b8e73c5766fd8151da8ca7
Denys Oblohin <denys.oblohin@okta.com>
2025-07-01 13:26:59 +0300

    clean setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,33 +14,11 @@ export PATH="${PATH}:$(yarn global bin)"
 
 # Revert the cache-min setting, since the internal cache does not apply to
 # these repos (and causes problems in lookups)
-#npm config set cache-min 10
+npm config set cache-min 10
 
 cd ${OKTA_HOME}/${REPO}
-
-# Yarn does not utilize the npmrc/yarnrc registry configuration
-# if a lockfile is present. This results in `yarn install` problems
-# for private registries. Until yarn@2.0.0 is released, this is our current
-# workaround.
-#
-# Related issues:
-#  - https://github.com/yarnpkg/yarn/issues/5892
-#  - https://github.com/yarnpkg/yarn/issues/3330
-
-YARN_REGISTRY=https://registry.yarnpkg.com
-OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-all
-
-# Replace yarn artifactory with Okta's
-#sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#g" yarn.lock
-
-# Override registry configs to point to the public registry since this repository is public
-# npm config set registry ${YARN_REGISTRY}
-# npm config set @okta:registry ${YARN_REGISTRY}
 
 if ! yarn install --frozen-lockfile; then
   echo "yarn install failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
-
-# Revert the original change
-#sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
