eb1065ea51ea4d756299d465df667043cfeb35b5
Denys Oblohin <denys.oblohin@okta.com>
2025-07-02 20:26:38 +0300

    add public key

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -7,6 +7,7 @@ export OKTA_CLIENT_ORGURL=https://node-sdk-oie.oktapreview.com
 export OKTA_CLIENT_CLIENTID=0oa1q34stxthm0zbJ1d7
 get_terminus_secret "/" api_key OKTA_CLIENT_TOKEN
 get_terminus_secret "/" private_key E2E_PK
+export OKTA_CLIENT_KEYID="vcwwhGUkVJbyKt3hu9DqKneDgjBD-IOQ-LLNNrtsCGg" # public key
 get_terminus_secret "/" username ORG_USER
 
 HEADER="-----BEGIN PRIVATE KEY-----"
