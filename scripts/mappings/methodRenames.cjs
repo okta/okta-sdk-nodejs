8abf591d35c9ef188d95d7f2c80f2ea96cfea189
Denys Oblohin <denys.oblohin@okta.com>
2025-07-03 19:16:49 +0300

    copied methodRenames, pathParamsRenmaes

--- scripts/mappings/methodRenames.cjs
+++ scripts/mappings/methodRenames.cjs
@@ -1,22 +1,22 @@
 // Please run `yarn build:validateGenerated` to fill this mapping
 const methodRenames = {
-  // new -> old
-  addGroup: 'createGroup',
-  listOrgContactTypes: 'getOrgContactTypes',
-  setOrgHideOktaUIFooter: 'updateOrgHideOktaUIFooter',
-  setOrgShowOktaUIFooter: 'updateOrgShowOktaUIFooter',
-  addResourceSetResources: 'addResourceSetResource',
-  listSubscriptionsRole: 'listRoleSubscriptions',
-  getSubscriptionsNotificationTypeRole: 'listRoleSubscriptionsByNotificationType',
-  listSubscriptionsUser: 'listUserSubscriptions',
-  getSubscriptionsNotificationTypeUser: 'listUserSubscriptionsByNotificationType',
-  subscribeByNotificationTypeRole: 'subscribeRoleSubscriptionByNotificationType',
-  subscribeByNotificationTypeUser: 'subscribeUserSubscriptionByNotificationType',
-  unsubscribeByNotificationTypeRole: 'unsubscribeRoleSubscriptionByNotificationType',
-  unsubscribeByNotificationTypeUser: 'unsubscribeUserSubscriptionByNotificationType',
-  expirePasswordWithTempPassword: 'expirePasswordAndGetTemporaryPassword',
-  resetPassword: 'generateResetPasswordToken',
-  assignLinkedObjectValueForPrimary: 'setLinkedObjectForUser'
+  // old -> new
+  createGroup: 'addGroup',
+  getOrgContactTypes: 'listOrgContactTypes',
+  updateOrgHideOktaUIFooter: 'setOrgHideOktaUIFooter',
+  updateOrgShowOktaUIFooter: 'setOrgShowOktaUIFooter',
+  addResourceSetResource: 'addResourceSetResources',
+  listRoleSubscriptions: 'listSubscriptionsRole',
+  listRoleSubscriptionsByNotificationType: 'getSubscriptionsNotificationTypeRole',
+  listUserSubscriptions: 'listSubscriptionsUser',
+  listUserSubscriptionsByNotificationType: 'getSubscriptionsNotificationTypeUser',
+  subscribeRoleSubscriptionByNotificationType: 'subscribeByNotificationTypeRole',
+  subscribeUserSubscriptionByNotificationType: 'subscribeByNotificationTypeUser',
+  unsubscribeRoleSubscriptionByNotificationType: 'unsubscribeByNotificationTypeRole',
+  unsubscribeUserSubscriptionByNotificationType: 'unsubscribeByNotificationTypeUser',
+  expirePasswordAndGetTemporaryPassword: 'expirePasswordWithTempPassword',
+  generateResetPasswordToken: 'resetPassword',
+  setLinkedObjectForUser: 'assignLinkedObjectValueForPrimary'
 };
 
 module.exports = {
