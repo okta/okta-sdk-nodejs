064838b11aa4f4ac081fb082e56094bc0143c527
Denys Oblohin <denys.oblohin@okta.com>
2025-07-11 13:34:44 +0300

    nit

--- test/it/authserver-assoc.ts
+++ test/it/authserver-assoc.ts
@@ -5,8 +5,6 @@ import {
   DefaultRequestExecutor,
 } from '@okta/okta-sdk-nodejs';
 import getMockAuthorizationServer = require('./mocks/authorization-server');
-import faker = require('@faker-js/faker');
-import utils = require('../utils');
 
 let orgUrl = process.env.OKTA_CLIENT_ORGURL;
 
