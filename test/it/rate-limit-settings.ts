51e20a6a45dbe1135f2c0e54b485bda2e8f480b3
Denys Oblohin <denys.oblohin@okta.com>
2025-07-11 10:43:57 +0300

    done rate-limit-dettings

--- test/it/rate-limit-settings.ts
+++ test/it/rate-limit-settings.ts
@@ -3,6 +3,8 @@ import {
   Client,
   DefaultRequestExecutor,
   PerClientRateLimitSettings,
+  RateLimitAdminNotifications,
+  RateLimitWarningThresholdResponse,
 } from '@okta/okta-sdk-nodejs';
 
 const orgUrl = process.env.OKTA_CLIENT_ORGURL;
@@ -16,5 +18,45 @@ describe('Rate Limit Settings API', () => {
   it('Get per-client rate limit settings', async () => {
     const rateLimitSettings = await client.rateLimitSettingsApi.getRateLimitSettingsPerClient({});
     expect(rateLimitSettings).to.be.instanceOf(PerClientRateLimitSettings);
+  });
+
+  it('Set per-client rate limit settings', async () => {
+    const rateLimitSettings = await client.rateLimitSettingsApi.getRateLimitSettingsPerClient({});
+    const newRateLimitSettings = await client.rateLimitSettingsApi.replaceRateLimitSettingsPerClient({
+      perClientRateLimitSettings: rateLimitSettings
+    });
+    expect(newRateLimitSettings).to.be.instanceOf(PerClientRateLimitSettings);
+    expect(newRateLimitSettings.defaultMode).to.equal(rateLimitSettings.defaultMode);
+    expect(newRateLimitSettings.useCaseModeOverrides).to.equal(rateLimitSettings.useCaseModeOverrides);
+  });
+
+  it('Get rate limit admin notification settings', async () => {
+    const rateLimitSettings = await client.rateLimitSettingsApi.getRateLimitSettingsAdminNotifications();
+    expect(rateLimitSettings).to.be.instanceOf(RateLimitAdminNotifications);
+  });
+
+  it('Set rate limit admin notification settings', async () => {
+    const rateLimitSettings = await client.rateLimitSettingsApi.getRateLimitSettingsAdminNotifications();
+    rateLimitSettings.notificationsEnabled = true;
+    const newRateLimitSettings = await client.rateLimitSettingsApi.replaceRateLimitSettingsAdminNotifications({
+      RateLimitAdminNotifications: rateLimitSettings
+    });
+    expect(newRateLimitSettings).to.be.instanceOf(RateLimitAdminNotifications);
+    expect(newRateLimitSettings.notificationsEnabled).to.equal(rateLimitSettings.notificationsEnabled);
+  });
+
+  it('Get threshold for warning notifications', async () => {
+    const rateLimitSettings = await client.rateLimitSettingsApi.getRateLimitSettingsWarningThreshold();
+    expect(rateLimitSettings).to.be.instanceOf(RateLimitWarningThresholdResponse);
+  });
+
+  it('Set threshold for warning notifications', async () => {
+    const newRateLimitSettings = await client.rateLimitSettingsApi.replaceRateLimitSettingsWarningThreshold({
+      rateLimitWarningThreshold: {
+        warningThreshold: 70
+      }
+    });
+    expect(newRateLimitSettings).to.be.instanceOf(RateLimitWarningThresholdResponse);
+    expect(newRateLimitSettings.warningThreshold).to.equal(70);
   });
 });
